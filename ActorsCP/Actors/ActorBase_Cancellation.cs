cfcfa5a1dfaca96b8ffb6da4d909b60f348a89f9
Andrey Khrenov <hm@inary.ru>
2021-09-10 16:28:27 +0300

    Дописан первый примитивный Cancel

---
+++ ActorsCP/Actors/ActorBase_Cancellation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorsCP.Actors
+    {
+    /// <summary>
+    /// Частичная реализация ActorBase
+    /// </summary>
+    public partial class ActorBase
+        {
+        #region Свойства
+
+        /// <summary>
+        ///
+        /// </summary>
+        // public string Property
+        //     {
+        //     get;
+        //     set;
+        //     }
+
+        #endregion Свойства
+
+        //
+        } // end class ActorBase
+    } // end namespace ActorsCP.Actors
