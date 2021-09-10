2a495a9ce12e5a6154de95363f77e16b4eeb8f6f
Andrey Khrenov <hm@inary.ru>
2021-09-10 16:38:16 +0300

    .

---
+++ ActorsCP/Actors/ActorsQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorsCP.Actors
+    {
+    /// <summary>
+    ///
+    /// </summary>
+    public class ActorsQueue : ActorBase, IActors
+        {
+        #region Конструкторы
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="parameter"></param>
+        public ActorsQueue() // (object parameter) : base(parameter)
+            {
+            }
+
+        #endregion Конструкторы
+
+        #region Свойства
+
+        /// <summary>
+        ///
+        /// </summary>
+        // public string Property
+        //     {
+        //     get;
+        //     set;
+        //     }
+
+        #endregion Свойства
+
+        //
+        } // end class ActorsQueue
+    } // end namespace ActorsCP.Actors
---
+++ ActorsCP/Actors/IActors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorsCP.Actors
+    {
+    /// <summary>
+    /// Интерфейс набора акторов
+    /// </summary>
+    internal interface IActors
+        {
+        } // end interface IActors
+    } // end namespace ActorsCP.Actors
