2a047ce7c29b08e9ebcf7e22432d8646654c23c0
Андрей Хренов <Andrey.Khrenov@gmail.com>
2021-09-12 23:13:13 +0300

    Опции актора

---
+++ ActorsCP/Helpers/SingletonT.cs
@@ -0,0 +1,27 @@
+﻿namespace ItMobileApp.General.Utils
+    {
+        /// <summary>
+        /// Шаблон синглтона
+        /// </summary>
+        /// <typeparam name="T">ссылочный объект с открытым конструктором по умолчанию.</typeparam>
+        public abstract class Singleton<T> where T : class, new()
+        {
+        /// <summary>
+        /// Единственный экзмепляр объекта
+        /// </summary>
+        private static T _instance;
+
+        /// <summary>
+        /// Получить экземпляр объекта
+        /// </summary>
+        /// <returns>единственный экземпляр</returns>
+        public static T GetInstance()
+            {
+            if (_instance == null)
+                {
+                _instance = new T();
+                }
+            return _instance;
+            }
+        }
+    }
---
+++ ActorsCP/Options/ActorOptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+
+namespace ActorsCP.Options
+    {
+    // http://dir.by/developer/csharp/concurrent_dictionary/add_or_update/
+
+    /// <summary>
+    /// Опции объекта
+    /// </summary>
+    public class ActorOptions : IActorOptions
+        {
+        private readonly ConcurrentDictionary<string, object> _bag = new ConcurrentDictionary<string, object>();
+
+        /// <summary>
+        /// Получить объект
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Get(string key, out object value)
+            {
+            var bres = _bag.TryGetValue(key, out value);
+            if (bres)
+                {
+                return true;
+                }
+
+            var gao = GlobalActorOptions.GetInstance();
+            bres = gao.Get(key, out value);
+            return bres;
+            }
+
+        public bool AddOrUpdate(string key, object value)
+            {
+            _bag.AddOrUpdate(key, value, (_key, oldValue) => value);
+            return true;
+            }
+        }
+    }
