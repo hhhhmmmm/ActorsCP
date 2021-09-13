65854be84b69efa6907c95603594f392ba4d8168
Андрей Хренов <Andrey.Khrenov@gmail.com>
2021-09-13 23:31:05 +0300

    Толпа вроде как работает

---
+++ ActorsCP.Unit.Test/ActorsCrowdTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks;
+
+using ActorsCP.Actors;
+using ActorsCP.TestActors;
+
+using NUnit.Framework;
+
+namespace ActorsCP.Unit.Test
+    {
+    /// <summary>
+    ///
+    /// </summary>
+    [TestFixture]
+    [Category("Тесты толпы")]
+    public sealed class ActorsCrowdTests : TestBase
+        {
+        /// <summary>
+        /// Инициализация
+        /// </summary>
+        [SetUp]
+        public void Init()
+            {
+            }
+
+        [Test]
+        [TestCase(TestName = "10. Пустая толпа")]
+        public Task EmptyCrowd()
+            {
+            var crowd = new ActorsCrowd();
+            Assert.AreEqual(0, crowd.WaitingCount);
+            Assert.AreEqual(0, crowd.RunningCount);
+            Assert.AreEqual(0, crowd.CompletedCount);
+            Assert.AreEqual(0, crowd.TotalCount);
+            return Task.CompletedTask;
+            }
+
+        [Test]
+        [TestCase(TestName = "11. Толпа из одного элемента")]
+        public async Task SimpleActorCrowd1()
+            {
+            var crowd = new ActorsCrowd();
+            crowd.SetCleanupAfterTermination(true);
+
+            Assert.AreEqual(0, crowd.WaitingCount);
+            Assert.AreEqual(0, crowd.RunningCount);
+            Assert.AreEqual(0, crowd.CompletedCount);
+            Assert.AreEqual(0, crowd.TotalCount);
+
+            const int nCount = 1;
+
+            for (int i = 0; i < nCount; i++)
+                {
+                string name = string.Format("Объект {0}", i + 1);
+                var actor = new SimpleActor(name);
+                crowd.Add(actor);
+                }
+
+            Assert.AreEqual(nCount, crowd.WaitingCount);
+            Assert.AreEqual(0, crowd.RunningCount);
+            Assert.AreEqual(0, crowd.CompletedCount);
+            Assert.AreEqual(nCount, crowd.TotalCount);
+
+            await crowd.RunAsync();
+
+            Assert.AreEqual(0, crowd.WaitingCount);
+            Assert.AreEqual(0, crowd.RunningCount);
+            Assert.AreEqual(nCount, crowd.CompletedCount);
+            Assert.AreEqual(nCount, crowd.TotalCount);
+            }
+        }
+    }
---
+++ ActorsCP/Actors/ActorsCrowd.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ActorsCP.Helpers;
+
+namespace ActorsCP.Actors
+    {
+    /// <summary>
+    /// Класс-набор - содержит в себе несколько объектов и обрабатывает их ОДНОВРЕМЕННО
+    /// </summary>
+    public class ActorsCrowd : ActorsSet
+        {
+        #region Конструкторы
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public ActorsCrowd()
+            {
+            SetName("Набор объектов");
+            }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="name">Название очереди объектов</param>
+        public ActorsCrowd(string name) : base(name)
+            {
+            }
+
+        #endregion Конструкторы
+
+        #region Перегруженные методы
+
+        /// <summary>
+        /// Перегружаемая функция для выполнения некоторых действий
+        /// </summary>
+        /// <returns>true если объект все выполнил успешно</returns>
+        protected override async Task<bool> InternalRunAsync()
+            {
+            bool bresult = true;
+            if (WaitingCount == 0)
+                {
+                return true;
+                }
+
+            var array = _waiting.ToArray();
+            await TasksHelper.ForEachAsyncConcurrentAutoAsync<ActorBase>(array, (x) => { return x.RunAsync(); });
+            return bresult;
+            }
+
+        #endregion Перегруженные методы
+
+        public static async Task ForEachAsyncConcurrentAsync<T>(
+            IEnumerable<T> enumerable,
+            Func<T, Task> action,
+            int? maxDegreeOfParallelism = null)
+            {
+            if (action == null)
+                {
+                throw new ArgumentNullException(nameof(action));
+                }
+
+            if (maxDegreeOfParallelism.HasValue)
+                {
+                using (var semaphoreSlim = new SemaphoreSlim(
+                    maxDegreeOfParallelism.Value, maxDegreeOfParallelism.Value))
+                    {
+                    var tasksWithThrottler = new List<Task>();
+
+                    foreach (var item in enumerable)
+                        {
+                        // Increment the number of currently running tasks and wait if they are more than limit.
+                        await semaphoreSlim.WaitAsync();
+
+                        tasksWithThrottler.Add(Task.Run(async () =>
+                        {
+                            await action(item).ContinueWith(res =>
+                            {
+                                // action is completed, so decrement the number of currently running tasks
+                                semaphoreSlim.Release();
+                            });
+                        }));
+                        }
+
+                    // Wait for all tasks to complete.
+                    await Task.WhenAll(tasksWithThrottler.ToArray());
+                    }
+                }
+            else
+                {
+                await Task.WhenAll(enumerable.Select(item => action(item)));
+                }
+            }
+        }
+    }
