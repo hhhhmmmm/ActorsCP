58edda1f326079cf6b798b107b73221aec389c87
Андрей Хренов <Andrey.Khrenov@gmail.com>
2021-09-13 19:53:51 +0300

    .

--- ActorsCP/Properties/AssemblyInfo.cs
+++ ActorsCP/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Библиотека объектов")]
-[assembly: AssemblyDescription("Собрано VS2019")]
+[assembly: AssemblyDescription("Собрано VS2019.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ActorsCP")]
