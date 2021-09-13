ca3b5d541fde0b6dba7be500b42ac781279c4bde
Andrey Khrenov <hm@inary.ru>
2021-09-13 12:34:32 +0300

    .

---
+++ ActorsCP.Unit.Test/ActorQueueTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+
+using ActorsCP.Actors;
+using ActorsCP.TestActors;
+
+using NUnit.Framework;
+
+namespace ActorsCP.Unit.Test
+    {
+    /// <summary>
+    ///
+    /// </summary>
+    [TestFixture]
+    [Category("Тесты очереди")]
+    public class ActorQueueTests : TestBase
+        {
+        #region Конструкторы
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="parameter"></param>
+        public ActorQueueTests() // (object parameter) : base(parameter)
+            {
+            }
+
+        #endregion Конструкторы
+
+        #region Свойства
+
+        /// <summary>
+        ///
+        /// </summary>
+        // public string Property
+        //     {
+        //     get;
+        //     set;
+        //     }
+
+        #endregion Свойства
+
+        //
+        } // end class ActorQueueTests
+    } // end namespace ActorsCP.Unit.Test
