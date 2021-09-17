fc4cb41441f8565727c9e3e5adc0c5815a413e7a
Андрей Хренов <Andrey.Khrenov@gmail.com>
2021-09-17 23:14:37 +0300

    Опции объекта вынесены в отдельный файл

--- ActorsCP/Actors/ActorBase_Options.cs
+++ ActorsCP/Actors/ActorBase_Options.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ActorsCP.Options;
 
 namespace ActorsCP.Actors
     {
@@ -9,5 +10,70 @@ namespace ActorsCP.Actors
     /// </summary>
     public partial class ActorBase
         {
+        #region Опции объекта
+
+        private IActorOptions _actorOptions = null;
+
+        /// <summary>
+        /// Опции объекта
+        /// </summary>
+        public IActorOptions Options
+            {
+            get
+                {
+                lock (Locker)
+                    {
+                    if (_actorOptions == null)
+                        {
+                        _actorOptions = new ActorOptions();
+                        }
+                    return _actorOptions;
+                    }
+                }
+            }
+
+        #endregion Опции объекта
+
+        #region Опции отладки объекта
+
+        private IActorOptions _actorDebugOptions = null;
+
+        /// <summary>
+        /// Опции отладки объекта
+        /// </summary>
+        public IActorOptions DebugOptions
+            {
+            get
+                {
+                lock (Locker)
+                    {
+                    if (_actorDebugOptions == null)
+                        {
+                        _actorDebugOptions = new ActorOptions();
+                        }
+                    return _actorDebugOptions;
+                    }
+                }
+            }
+
+        #endregion Опции отладки объекта
+
+        /// <summary>
+        /// Установить персональные опции объекта
+        /// </summary>
+        /// <param name="actorOptions">Опции объекта</param>
+        public void SetActorOptions(IActorOptions actorOptions)
+            {
+            _actorOptions = actorOptions;
+            }
+
+        /// <summary>
+        /// Установить персональные опции отладки объекта
+        /// </summary>
+        /// <param name="actorDebugOptions">Опции отладки объекта</param>
+        public void SetActorDebugOptions(IActorOptions actorDebugOptions)
+            {
+            _actorDebugOptions = actorDebugOptions;
+            }
         }
     }
