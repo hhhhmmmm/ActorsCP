d4ca5bec2e5870dd0c1f1ba180c4241c15bc2bc3
Андрей Хренов <Andrey.Khrenov@gmail.com>
2021-09-17 22:58:41 +0300

    Добавлены отладочные опции DebugOptions

---
+++ ActorsCP/Actors/ActorBase_Options.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorsCP.Actors
+    {
+    /// <summary>
+    /// Частичная реализация ActorBase
+    /// </summary>
+    public partial class ActorBase
+        {
+        }
+    }
---
+++ ActorsCP/Options/GlobalActorDebugOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ActorsCP.Helpers;
+
+namespace ActorsCP.Options
+    {
+    /// <summary>
+    /// Глобальные настройки отладки объектов
+    /// </summary>
+    public sealed class GlobalActorDebugOptions : SingletonT<ActorOptions>
+        {
+        }
+    }
