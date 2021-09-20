85e81f0f3d3f3242c40982707b50d83b5b833749
Andrey Khrenov <hm@inary.ru>
2021-09-20 14:57:23 +0300

    Будем считать, что с логгером закончили

---
+++ ActorsCP/Logger/DefaultErrorLoggerOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorsCP.Logger
+    {
+    /// <summary>
+    /// Преднастроенные опции логгера
+    /// </summary>
+    public static class DefaultErrorLoggerOptions
+        {
+        /// <summary>
+        /// Логгер уровня Ошибки и выше
+        /// </summary>
+        public static ActorLoggerOptions ErrorLoggerOptions
+            {
+            get;
+            } = new ActorLoggerOptions(ActorLogLevel.Error);
+        } // end class DefaultErrorLoggerOptions
+    } // end namespace ActorsCP.Logger
