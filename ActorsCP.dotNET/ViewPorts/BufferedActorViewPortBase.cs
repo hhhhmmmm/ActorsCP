5e60859ab21ccd541876217459861169a161e55e
Андрей Хренов <Andrey.Khrenov@gmail.com>
2021-09-21 22:55:17 +0300

    Добавлена библиотека ActorsCP.dotNET

---
+++ ActorsCP.dotNET/ViewPorts/BufferedActorViewPortBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActorsCP.ViewPorts;
+
+namespace ActorsCP.dotNET.ViewPorts
+    {
+    /// <summary>
+    /// Базовый класс вьюпорта со всеми интерфейсами
+    /// и буферизованным выводом
+    /// </summary>
+    public class BufferedActorViewPortBase : ActorViewPortBase
+        {
+        }
+    }
