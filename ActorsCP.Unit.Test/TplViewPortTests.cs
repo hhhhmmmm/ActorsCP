3c407097d20414a759318899df78c446dc707d70
Andrey Khrenov <hm@inary.ru>
2021-09-24 13:38:55 +0300

    Добавлен буфер в виде очереди, сделан используемым по умолчанию

--- ActorsCP.Unit.Test/TplViewPortTests.cs
+++ ActorsCP.Unit.Test/TplViewPortTests.cs
@@ -21,11 +21,13 @@ namespace ActorsCP.Unit.Test
             }
 
         [Test]
-        [TestCase(TestName = "10. Простой Tpl вьюпорт - старт/стоп")]
-        public void SimpleActorTest()
+        [TestCase(true, TestName = "10. Простой Tpl вьюпорт - старт/стоп")]
+        [TestCase(false, TestName = "10. Простой Queue вьюпорт - старт/стоп")]
+        public void SimpleActorTest(bool useQueueForBuffering)
             {
             using (var viewport = new BufferedActorViewPortBase())
                 {
+                viewport.UseQueueForBuffering = useQueueForBuffering;
                 viewport.Init();
                 Assert.IsTrue(viewport.IsInitialized);
                 viewport.Terminate();
@@ -36,18 +38,27 @@ namespace ActorsCP.Unit.Test
             }
 
         [Test]
-        [TestCase(1, TestName = "20. 1 - Tpl вьюпорт")]
-        [TestCase(10, TestName = "30. 10 - Tpl вьюпорт")]
-        [TestCase(1000, TestName = "40. 1000 - Tpl вьюпорт")]
-        [TestCase(10000, TestName = "50. 10000 - Tpl вьюпорт")]
-        [TestCase(50000, TestName = "60. 50000 - Tpl вьюпорт")]
-        [TestCase(75000, TestName = "70. 75000 - Tpl вьюпорт")]
-        [TestCase(100000, TestName = "80. 100000 - Tpl вьюпорт")]
-        [TestCase(300000, TestName = "90. 300000 - Tpl вьюпорт")]
-        public async Task SimpleActorTest2(int N)
+        [TestCase(1, false, TestName = "20. 1 - Tpl вьюпорт")]
+        [TestCase(1, true, TestName = "20. 1 - Queue вьюпорт")]
+        [TestCase(10, false, TestName = "30. 10 - Tpl вьюпорт")]
+        [TestCase(10, true, TestName = "30. 10 - Queue вьюпорт")]
+        [TestCase(1000, false, TestName = "40. 1000 - Tpl вьюпорт")]
+        [TestCase(1000, true, TestName = "40. 1000 - Queue вьюпорт")]
+        [TestCase(10000, false, TestName = "50. 10000 - Tpl вьюпорт")]
+        [TestCase(10000, true, TestName = "50. 10000 - Queue вьюпорт")]
+        [TestCase(50000, false, TestName = "60. 50000 - Tpl вьюпорт")]
+        [TestCase(50000, true, TestName = "60. 50000 - Queue вьюпорт")]
+        [TestCase(75000, false, TestName = "70. 75000 - Tpl вьюпорт")]
+        [TestCase(75000, true, TestName = "70. 75000 - Queue вьюпорт")]
+        [TestCase(100000, false, TestName = "80. 100000 - Tpl вьюпорт")]
+        [TestCase(100000, true, TestName = "80. 100000 - Queue вьюпорт")]
+        [TestCase(300000, false, TestName = "90. 300000 - Tpl вьюпорт")]
+        [TestCase(300000, true, TestName = "90. 300000 - Queue вьюпорт")]
+        public void SimpleActorTest2(int N, bool useQueueForBuffering)
             {
             using (var viewport = new BufferedActorViewPortBase())
                 {
+                viewport.UseQueueForBuffering = useQueueForBuffering;
                 viewport.Init();
                 Assert.IsTrue(viewport.IsInitialized);
 
@@ -56,7 +67,7 @@ namespace ActorsCP.Unit.Test
                     {
                     var actor = base.TestActorsBuilder.NewSimpleActor;
                     var viewPortItem = new ViewPortItem(actor, ea);
-                    viewport.TplDataFlowAddData(viewPortItem);
+                    viewport.Add(viewPortItem);
                     }
 
                 viewport.Terminate();
@@ -64,9 +75,9 @@ namespace ActorsCP.Unit.Test
 
                 var stat = viewport.СurrentExecutionStatistics;
 
-                Assert.AreEqual(stat.TplAddedMessages, stat.TplProcessedMessages);
-                Assert.AreEqual(N, stat.TplAddedMessages);
-                Assert.AreEqual(N, stat.TplProcessedMessages);
+                Assert.AreEqual(stat.BufferedAddedMessages, stat.BufferedProcessedMessages);
+                Assert.AreEqual(N, stat.BufferedAddedMessages);
+                Assert.AreEqual(N, stat.BufferedProcessedMessages);
                 viewport.ValidateStatistics();
                 }
             }
