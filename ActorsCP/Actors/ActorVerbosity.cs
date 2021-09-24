f6670c81159b287ddef2e9d1a8a919546def375f
Andrey Khrenov <hm@inary.ru>
2021-09-24 15:55:05 +0300

    Добавлена ActorVerbosity

---
+++ ActorsCP/Actors/ActorVerbosity.cs
@@ -0,0 +1,38 @@
+﻿namespace ActorsCP.Actors
+    {
+    /// <summary>
+    /// Многословность объекта
+    /// </summary>
+    public enum ActorVerbosity
+        {
+        /// <summary>
+        /// Работаем молча
+        /// </summary>
+        Off = 0,
+
+        /// <summary>
+        /// Запускается...
+        /// </summary>
+        Starting = 0,
+
+        /// <summary>
+        /// Запущен
+        /// </summary>
+        Started = 1,
+
+        /// <summary>
+        /// Останавливается
+        /// </summary>
+        Stopping = 2,
+
+        /// <summary>
+        /// Остановлен
+        /// </summary>
+        Stopped = 3,
+
+        ///// <summary>
+        ///// Завершен навсегда
+        ///// </summary>
+        //Terminated = 4
+        } // end enum ActorState
+    } // end namespace ActorsCP.Actors
---
+++ ActorsCP/Options/ActorKeywords.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorsCP.Options
+    {
+    /// <summary>
+    /// Словарь слов понимаемых объектами
+    /// </summary>
+    public static class ActorKeywords
+        {
+        /// <summary>
+        /// Многословность объекта - о каких событиях он будет сообщать
+        /// </summary>
+        public const string ActorVerbosity = "ActorVerbosity";
+        } // end class ActorKeywords
+    } // end namespace ActorsCP.Options
