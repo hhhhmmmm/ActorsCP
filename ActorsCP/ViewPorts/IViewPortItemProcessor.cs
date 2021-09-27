68916d7019c7875e4bd6fccfb4b9a04112a94bef
Andrey Khrenov <hm@inary.ru>
2021-09-27 17:15:30 +0300

    .

---
+++ ActorsCP/ViewPorts/IViewPortItemProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorsCP.ViewPorts
+    {
+    /// <summary>
+    /// Обработчик элемента ViewPortItem
+    /// </summary>
+    public interface IViewPortItemProcessor
+        {
+        } // end interface IViewPortItemProcessor
+    } // end namespace ActorsCP.ViewPorts
