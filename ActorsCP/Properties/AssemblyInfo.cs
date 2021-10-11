f3e47f8f71655a4f7194d59dcc8664c767a7b505
Андрей Хренов <Andrey.Khrenov@gmail.com>
2021-10-11 08:11:01 +0300

    .

--- ActorsCP/Properties/AssemblyInfo.cs
+++ ActorsCP/Properties/AssemblyInfo.cs
@@ -1,37 +1,15 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Библиотека объектов")]
-[assembly: AssemblyDescription("Собрано VS2019")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ActorsCP")]
-[assembly: AssemblyCopyright("Copyright ©  2021 Андрей Хренов")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("88f06a54-20ed-4cd2-b8fd-8f6ab10cbe9c")]
+// This is generated code. Any changes will be lost the next time this code is being generated.
+// Creation date: 10.10.2021 21:45:15
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("21.10.09.0")]
 
-// Можно менять, это не оказывает влияния на совместимость в exe файлом
-[assembly: AssemblyFileVersion("21.10.09.0")]
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("21.10.10.2145")]
+[assembly: AssemblyFileVersion("21.10.10.2145")]
+
+
